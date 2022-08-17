f539887c4b14adaf76dcb1663067d9830336f044
Michael Park <tjp4ca@gmail.com>
2022-08-17 17:53:25 -0400

    category route

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use(routes);
 //   console.log(`App listening on port ${PORT}!`);
 // });
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
